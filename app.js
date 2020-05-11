3233bf10496ad21f5bd83f74a86fefd796bb98b3
Randeep Singh <randeeprattan90@gmail.com>
2020-05-11 22:14:48 +0100

    Generate random color on page load

---
+++ app.js
@@ -0,0 +1,27 @@
+// global selections and vars
+const colorDivs = document.querySelectorAll(".color");
+const generateButton = document.querySelector(".generate");
+const sliders = document.querySelectorAll('input[type="range"]');
+const currentHexes = document.querySelectorAll(".color h2");
+let initialColors;
+
+// functions
+
+// color generator
+function generateHex() {
+  const hexColor = chroma.random();
+  return hexColor;
+}
+
+function randomColors() {
+  colorDivs.forEach((div, index) => {
+    const hexText = div.children[0];
+    const randomColor = generateHex();
+
+    //add color to background and set h tag
+    div.style.backgroundColor = randomColor;
+    hexText.innerText = randomColor;
+  });
+}
+
+randomColors();
