faf21a6e18cd94f69ad0bc00f98f0420d42712aa
Randeep Singh <randeeprattan90@gmail.com>
2020-05-17 16:41:16 +0100

    Update slider values

--- app.js
+++ app.js
@@ -44,6 +44,8 @@ function randomColors() {
 
     colorizeSliders(color, hue, brightness, saturation);
   });
+  //reset inputs
+  resetInputs();
 }
 
 function checkTextContrast(color, text) {
@@ -106,4 +108,27 @@ function updateTextUI(index) {
     checkTextContrast(color, icon);
   }
 }
+
+function resetInputs() {
+  const sliders = document.querySelectorAll(".sliders input");
+  sliders.forEach((slider) => {
+    if (slider.name === "hue") {
+      const hueColor = initialColors[slider.getAttribute("data-hue")];
+      const hueValue = chroma(hueColor).hsl()[0];
+      slider.value = Math.floor(hueValue);
+    }
+    if (slider.name === "brightness") {
+      const brightColor = initialColors[slider.getAttribute("data-bright")];
+      const brightValue = chroma(brightColor).hsl()[2];
+      slider.value = Math.floor(brightValue * 100) / 100;
+    }
+
+    if (slider.name === "saturation") {
+      const satColor = initialColors[slider.getAttribute("data-sat")];
+      const satValue = chroma(satColor).hsl()[1];
+      slider.value = Math.floor(satValue * 100) / 100;
+    }
+  });
+}
+
 randomColors();
